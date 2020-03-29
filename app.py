3895a31629cb76c139868c118f116c5dfffe8cd8
xu <yanxuchen@yanxuchens-MacBook-Air.local>
2020-03-30 01:07:41 +0800

    location

--- app.py
+++ app.py
@@ -38,9 +38,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = ImageSendMessage(
-    original_content_url='https://example.com/original.jpg',
-    preview_image_url='https://example.com/preview.jpg'
+    s = LocationSendMessage(
+    title='my location',
+    address='Tokyo',
+    latitude=35.65910807942215,
+    longitude=139.70372892916203
     )
     line_bot_api.reply_message(
         event.reply_token,
