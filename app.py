8ce9bd0260894ca202b64f82c89c11fbd75dab9b
xu <yanxuchen@yanxuchens-MacBook-Air.local>
2020-03-30 00:43:27 +0800

    textmsg

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    message = TextSendMessage(text='拜託成功')
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
